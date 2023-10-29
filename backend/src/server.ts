804e03ad8cb1e626b9006658a10627b5ebf4e30c
raphaelbat <113071983+raphaelbat@users.noreply.github.com>
2023-10-28 21:54:47 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import gracefulShutdown from "http-graceful-shutdown";
+import app from "./app";
+import { initIO } from "./libs/socket";
+import { logger } from "./utils/logger";
+import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
+import Company from "./models/Company";
+import { startQueueProcess } from "./queues";
+
+const server = app.listen(process.env.PORT, async () => {
+  const companies = await Company.findAll();
+  const allPromises: any[] = [];
+  companies.map(async c => {
+    const promise = StartAllWhatsAppsSessions(c.id);
+    allPromises.push(promise);
+  });
+
+  Promise.all(allPromises).then(() => {
+    startQueueProcess();
+  });
+  logger.info(`Server started on port: ${process.env.PORT}`);
+});
+
+initIO(server);
+gracefulShutdown(server);
---
+++ backend/src/services/WhatsappService/UpdateWhatsAppService.ts
@@ -0,0 +1,108 @@
+import * as Yup from "yup";
+import { Op } from "sequelize";
+
+import AppError from "../../errors/AppError";
+import Whatsapp from "../../models/Whatsapp";
+import ShowWhatsAppService from "./ShowWhatsAppService";
+import AssociateWhatsappQueue from "./AssociateWhatsappQueue";
+
+interface WhatsappData {
+  name?: string;
+  status?: string;
+  session?: string;
+  isDefault?: boolean;
+  greetingMessage?: string;
+  complationMessage?: string;
+  outOfHoursMessage?: string;
+  ratingMessage?: string;
+  queueIds?: number[];
+  token?: string;
+  sendIdQueue?: number;
+  timeSendQueue?: number;
+}
+
+interface Request {
+  whatsappData: WhatsappData;
+  whatsappId: string;
+  companyId: number;
+}
+
+interface Response {
+  whatsapp: Whatsapp;
+  oldDefaultWhatsapp: Whatsapp | null;
+}
+
+const UpdateWhatsAppService = async ({
+  whatsappData,
+  whatsappId,
+  companyId
+}: Request): Promise<Response> => {
+  const schema = Yup.object().shape({
+    name: Yup.string().min(2),
+    status: Yup.string(),
+    isDefault: Yup.boolean()
+  });
+
+  const {
+    name,
+    status,
+    isDefault,
+    session,
+    greetingMessage,
+    complationMessage,
+    outOfHoursMessage,
+    ratingMessage,
+    queueIds = [],
+    token,
+    timeSendQueue,
+    sendIdQueue = null,
+  } = whatsappData;
+
+  try {
+    await schema.validate({ name, status, isDefault });
+  } catch (err: any) {
+    throw new AppError(err.message);
+  }
+
+  if (queueIds.length > 1 && !greetingMessage) {
+    throw new AppError("ERR_WAPP_GREETING_REQUIRED");
+  }
+
+  let oldDefaultWhatsapp: Whatsapp | null = null;
+
+  if (isDefault) {
+    oldDefaultWhatsapp = await Whatsapp.findOne({
+      where: {
+        isDefault: true,
+        id: { [Op.not]: whatsappId },
+        companyId
+      }
+    });
+    if (oldDefaultWhatsapp) {
+      await oldDefaultWhatsapp.update({ isDefault: false });
+    }
+  }
+
+  const whatsapp = await ShowWhatsAppService(whatsappId, companyId);
+
+  await whatsapp.update({
+    name,
+    status,
+    session,
+    greetingMessage,
+    complationMessage,
+    outOfHoursMessage,
+    ratingMessage,
+    isDefault,
+    companyId,
+    token,
+    timeSendQueue,
+    sendIdQueue,
+  });
+
+  await AssociateWhatsappQueue(whatsapp, queueIds);
+
+  return { whatsapp, oldDefaultWhatsapp };
+};
+
+export default UpdateWhatsAppService;
